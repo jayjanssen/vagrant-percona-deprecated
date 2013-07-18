aff73733f4918f57aca517a79dc6c90ff18e52ec
Jay Janssen <jay.janssen@gmail.com>
2013-07-18 10:26:05 -0400

    Add sst user grant

--- puppet/modules/percona/manifests/cluster.pp
+++ puppet/modules/percona/manifests/cluster.pp
@@ -37,5 +37,12 @@ class percona::cluster {
 			require => Package['MySQL-server'],
 	}
 	
+	exec { 'xtrabackup_sst_grant_node1':
+    command => "/usr/bin/mysql -u root -e \"GRANT RELOAD, LOCK TABLES, REPLICATION CLIENT ON *.* TO 'sst'@'localhost' IDENTIFIED BY 'secret'\"",  
+		path => "/usr/bin:/usr/sbin:/bin:/sbin",
+    unless => "/usr/bin/mysql -u root -e\"SELECT User from mysql.user WHERE User='sst';\" | /bin/grep -q sst",
+		require => Service['mysql'];
+	}
+	
 	
 }
