13c25ea96d40fa6a9524ac8cc797a42c7547e138
Jay Janssen <jay.janssen@gmail.com>
2013-07-24 11:11:31 -0400

    fix sysbench install, add sysbench_tests symlink

--- puppet/modules/misc/manifests/sysbench.pp
+++ puppet/modules/misc/manifests/sysbench.pp
@@ -1,9 +1,15 @@
 class misc::sysbench {	
 	exec {
 		"sysbench":
-			command => "/usr/bin/yum localinstall -y /vagrant/modules/misc/files/sysbench-0.5-3.el6.i386.rpm",
+			command => "/usr/bin/yum localinstall -y /vagrant/puppet/modules/misc/files/sysbench-0.5-3.el6_.x86_64.rpm",
 			cwd => "/tmp",
 			unless => "/bin/rpm -q sysbench",
 			require => Package['MySQL-shared-compat'];
+	}
+	file {
+		"/root/sysbench_tests":
+			ensure => link,
+			target => '/usr/share/doc/sysbench/tests',
+			require => Exec['sysbench'];
 	}
 }
