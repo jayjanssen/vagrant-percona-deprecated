ba8f8e0a7ee66f631582b6a8942240f370ee6f25
Jay Janssen <jay.janssen@gmail.com>
2013-08-20 10:43:52 -0400

    tpcc_mysql setup module

---
+++ puppet/modules/misc/manifests/tpcc_mysql.pp
@@ -0,0 +1,28 @@
+class misc::tpcc_mysql {	
+	exec {
+		"build-essentials":
+			command => '/usr/bin/yum groupinstall "Development Tools" -y',
+			cwd => "/tmp",
+			unless => "/bin/rpm -q make";
+	}
+	package { 
+		"openssl-devel": ensure => 'installed'; 
+		"bzr": ensure => 'installed'; 
+	}
+	
+	
+	exec {
+		"tpcc_checkout":
+			command => "bzr branch lp:~percona-dev/perconatools/tpcc-mysql",
+			cwd => "/root",
+			path => "/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin",
+			require => Package['bzr'],
+			unless => "test -d /root/tpcc-mysql";
+		"tpcc_build":
+			command => 'make all',
+			cwd => "/root/tpcc-mysql",
+			path => "/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin",
+			require => [ Package['openssl-devel'], Exec['build-essentials', 'tpcc_checkout']],
+			unless => "test -f /root/tpcc-mysql/tpcc_load";
+	}
+}
