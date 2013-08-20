ccedaa8d5d4680eee87fd089aaa7651fe154e465
Jay Janssen <jay.janssen@gmail.com>
2013-08-20 08:13:20 -0400

    Include sysbench

--- puppet/manifests/init.pp
+++ puppet/manifests/init.pp
@@ -4,6 +4,8 @@ include percona::server
 include percona::config
 
 include misc
+include misc::sysbench
+
 
 Class['percona::repository'] -> Class['percona::config'] -> Class['percona::server']
 Class['percona::repository'] -> Class['percona::toolkit']
