839058b7e674e37e2020063f58893428cefe6116
Jay Janssen <jay.janssen@gmail.com>
2013-08-22 10:19:56 -0400

    moved sst GRANT to bootstrap script to simplify puppet config

--- puppet/modules/percona/manifests/cluster.pp
+++ puppet/modules/percona/manifests/cluster.pp
@@ -3,8 +3,7 @@ class percona::cluster {
 	include percona::cluster::packages
 	include percona::cluster::config  
 	include percona::cluster::service    
-	include percona::cluster::sst_config
 
-	Class['percona::cluster::packages'] -> Class['percona::cluster::config'] -> Class['percona::cluster::service'] -> Class['percona::cluster::sst_config']
+	Class['percona::cluster::packages'] -> Class['percona::cluster::config'] -> Class['percona::cluster::service']
 
 }
