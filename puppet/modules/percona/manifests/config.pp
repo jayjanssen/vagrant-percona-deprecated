f5360c350ad29e1054029327ff420f023cc0a7f0
Jay Janssen <jay.janssen@gmail.com>
2013-08-22 09:37:23 -0400

    remove /mnt/data from config manifest

--- puppet/modules/percona/manifests/config.pp
+++ puppet/modules/percona/manifests/config.pp
@@ -3,12 +3,5 @@ class percona::config {
 		"/etc/my.cnf":
 			ensure  => present,
 			content => template("percona/my.cnf.erb"),
-	}             
-	
-	file {
-		"/mnt/data":
-			ensure => directory,
-			mode => 0755,
-			require => Exec['rebuild-xvdb'];
-	}         
+	}                     
 }
