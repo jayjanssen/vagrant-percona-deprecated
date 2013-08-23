8d579fe8297cfd3d4c245655ffe8f4ef9794b007
Jay Janssen <jay.janssen@gmail.com>
2013-08-23 10:21:05 -0400

    fixed sysbench rpm install to be agnostic to vagrant dir location

--- puppet/modules/misc/manifests/sysbench.pp
+++ puppet/modules/misc/manifests/sysbench.pp
@@ -1,12 +1,15 @@
 class misc::sysbench {	
 	exec {
 		"sysbench":
-			command => "/usr/bin/yum localinstall -y /vagrant/puppet/modules/misc/files/sysbench-0.5-3.el6_.x86_64.rpm",
+			command => "/usr/bin/yum localinstall -y /tmp/sysbench.rpm",
 			cwd => "/tmp",
 			unless => "/bin/rpm -q sysbench",
-			require => Package['MySQL-shared-compat'];
+			require => [Package['MySQL-shared-compat'], File['/tmp/sysbench.rpm']];
 	}
 	file {
+		"/tmp/sysbench.rpm":
+			source => "puppet:///modules/misc/sysbench-0.5-3.el6_.x86_64.rpm",
+			ensure => present;
 		"/root/sysbench_tests":
 			ensure => link,
 			target => '/usr/share/doc/sysbench/tests',
