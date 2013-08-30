d3306dee31f015cdb195dde549af390169921fda
Jay Janssen <jay.janssen@gmail.com>
2013-08-30 15:22:14 -0400

    set default values for some galera settings

--- puppet/modules/percona/manifests/cluster/config.pp
+++ puppet/modules/percona/manifests/cluster/config.pp
@@ -1,4 +1,12 @@
 class percona::cluster::config {
+
+	if( $wsrep_provider_options == undef ) {
+		$wsrep_provider_options = ""
+	}
+	if( $wsrep_slave_threads == undef ) {
+		$wsrep_slave_threads = 4
+	}
+
 	file {
 		"/etc/my.cnf":
 			ensure  => present,
