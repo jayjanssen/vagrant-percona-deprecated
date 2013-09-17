aa09bc93ca86f051815ce8e978fd1a8b21681c72
Jay Janssen <jay.janssen@gmail.com>
2013-09-17 14:24:39 -0400

    cluster can remove server

---
+++ puppet/modules/percona/manifests/cluster/remove_server.pp
@@ -0,0 +1,31 @@
+class percona::cluster::remove_server {
+	package {
+		"Percona-Server-server-55.$hardwaremodel":
+			require => Yumrepo['percona'],
+			ensure => 'absent';
+		"Percona-Server-client-55.$hardwaremodel":
+			require => Yumrepo['percona'],
+			ensure => 'absent';
+		"mysql-libs":
+			ensure => "absent";
+		"Percona-Server-shared-55.$hardwaremodel":
+			require => Yumrepo['percona'],    
+			ensure => 'absent'; 
+		"Percona-Server-devel-55.$hardwaremodel":
+			ensure => 'absent';
+	}    
+
+	exec { 
+		'remove_master_info':
+			command => "rm -f /var/lib/mysql/master.info",    
+			path => "/usr/bin:/usr/sbin:/bin:/sbin",
+			onlyif => [
+				 "test -f /var/lib/mysql/master.info"
+			 ];
+		'remove_sysbench':
+			command => "rpm -e sysbench",
+			path => "/usr/bin:/usr/sbin:/bin:/sbin";
+	}      
+
+	Exec['remove_sysbench'] -> Package["Percona-Server-devel-55.$hardwaremodel"] -> Package["Percona-Server-server-55.$hardwaremodel"] -> Package["Percona-Server-client-55.$hardwaremodel"] -> Package["Percona-Server-shared-55.$hardwaremodel"] -> Exec['remove_master_info']
+}
