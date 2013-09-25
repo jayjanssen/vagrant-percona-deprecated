24238bec653a46bd0bf2bbc7adaf0a6a4ceda08d
Jay Janssen <jay.janssen@gmail.com>
2013-09-25 10:44:19 -0700

    pxc multi-region bootstrap works

--- pxc-bootstrap.sh
+++ pxc-bootstrap.sh
@@ -4,37 +4,51 @@
 # Bootstrap the cluster after 'vagrant up'.  This is required because we won't know the IPs of the nodes until then (on AWS).
 nic='eth1'
 using_aws=false
+get_ip_cmd="ip a l | grep $nic | grep inet | awk '{print \\$2}' | awk -F/ '{print \\$1}'"
+
 if vagrant status | grep aws > /dev/null
 then
+	echo "AWS detected: using public hostnames"
 	using_aws=true
-	nic='eth0'
+	get_ip_cmd="curl -s http://169.254.169.254/latest/meta-data/public-hostname"
+else
+	echo "Assuming $nic is the Galera communication nic"
 fi
 
-echo "Assuming $nic is the Galera communication nic"
+node1_ip=`vagrant ssh node1 -c "$get_ip_cmd"`
+echo "Node1: $node1_ip";
+node2_ip=`vagrant ssh node2 -c "$get_ip_cmd"`
+echo "Node2: $node2_ip";
+node3_ip=`vagrant ssh node3 -c "$get_ip_cmd"`
+echo "Node3: $node3_ip";
 
 
-node1_ip=`vagrant ssh node1 -c "ip a l | grep $nic | grep inet | awk '{print \\$2}' | awk -F/ '{print \\$1}'"`
-echo "Node1's ip: $node1_ip";
-node2_ip=`vagrant ssh node2 -c "ip a l | grep $nic | grep inet | awk '{print \\$2}' | awk -F/ '{print \\$1}'"`
-echo "Node2's ip: $node2_ip";
-node3_ip=`vagrant ssh node3 -c "ip a l | grep $nic | grep inet | awk '{print \\$2}' | awk -F/ '{print \\$1}'"`
-echo "Node3's ip: $node3_ip";
-
+echo "Adding configuration to /etc/my-pxc.cnf on all nodes"
 wsrep_cluster_address="echo -e \"[mysqld]\n\nwsrep_cluster_address = gcomm://$node1_ip,$node2_ip,$node3_ip\n\" > /etc/my-pxc.cnf"
-echo $wsrep_cluster_address
 
 vagrant ssh node1 -c "$wsrep_cluster_address"
 vagrant ssh node2 -c "$wsrep_cluster_address"
 vagrant ssh node3 -c "$wsrep_cluster_address"
 
-if ! $using_aws
+if $using_aws
 then
+	# Set the SST and IST receive addresses too
+	echo "AWS: setting SST and IST receive addresses"
+	vagrant ssh node1 -c "echo -e \"wsrep_sst_receive_address = $node1_ip\n\" >> /etc/my-pxc.cnf"
+	vagrant ssh node2 -c "echo -e \"wsrep_sst_receive_address = $node2_ip\n\" >> /etc/my-pxc.cnf"
+	vagrant ssh node3 -c "echo -e \"wsrep_sst_receive_address = $node3_ip\n\" >> /etc/my-pxc.cnf"
+
+	vagrant ssh node1 -c "echo -e \"wsrep_provider_options = 'ist.recv_addr=$node1_ip'\n\" >> /etc/my-pxc.cnf"
+	vagrant ssh node2 -c "echo -e \"wsrep_provider_options = 'ist.recv_addr=$node2_ip'\n\" >> /etc/my-pxc.cnf"
+	vagrant ssh node3 -c "echo -e \"wsrep_provider_options = 'ist.recv_addr=$node3_ip'\n\" >> /etc/my-pxc.cnf"
+else
 	# Set the wsrep_node_address too
+	echo "Setting wsrep_node_address"
 	vagrant ssh node1 -c "echo -e \"wsrep_node_address = $node1_ip\n\" >> /etc/my-pxc.cnf"
 	vagrant ssh node2 -c "echo -e \"wsrep_node_address = $node2_ip\n\" >> /etc/my-pxc.cnf"
 	vagrant ssh node3 -c "echo -e \"wsrep_node_address = $node3_ip\n\" >> /etc/my-pxc.cnf"
-
 fi
+
 
 vagrant ssh node1 -c "service mysql stop; service mysql bootstrap-pxc"
 # Setup SST user on node1
