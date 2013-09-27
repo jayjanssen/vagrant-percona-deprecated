cfa89696e3e4b67f0ced2e623daa49eb548aa707
Kenny Gryp <gryp@dakin.be>
2013-09-27 12:50:38 +0200

    Issue 3: move the ugly comment close to the code it talks about

--- puppet/modules/percona/manifests/server.pp
+++ puppet/modules/percona/manifests/server.pp
@@ -1,12 +1,11 @@
 class percona::server {
-	# ugly way of making sure the version we want to use doesn't conflict with the old one
-	# (oh boy this whole thing might need refactoring)
-	
 	# Default PS version is 55 for now
 	if( $percona_server_version == undef ) {
 		$percona_server_version = '55'
 	}
 	
+	# ugly way of making sure the version we want to use doesn't conflict with the old one
+	# (oh boy this whole thing might need refactoring)
 	if $percona_server_version == 55 {
 		$other_percona_server_version="56"
 	} elsif $percona_server_version == 56 {
