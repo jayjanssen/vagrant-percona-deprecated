4509e5e1d113ad17b999e1d36fd4e4070133cd90
Jay Janssen <jay.janssen@gmail.com>
2013-10-04 15:55:52 -0400

    dont install sysbench on ubuntu for now -- no pkg

--- puppet/modules/misc/manifests/sysbench.pp
+++ puppet/modules/misc/manifests/sysbench.pp
@@ -1,18 +1,25 @@
 class misc::sysbench {	
-	exec {
-		"sysbench":
-			command => "/usr/bin/yum localinstall -y /tmp/sysbench.rpm",
-			cwd => "/tmp",
-			unless => "/bin/rpm -q sysbench",
-			require => [Package['MySQL-shared-compat'], File['/tmp/sysbench.rpm']];
-	}
-	file {
-		"/tmp/sysbench.rpm":
-			source => "puppet:///modules/misc/sysbench-0.5-3.el6_.x86_64.rpm",
-			ensure => present;
-		"/root/sysbench_tests":
-			ensure => link,
-			target => '/usr/share/doc/sysbench/tests',
-			require => Exec['sysbench'];
+	case $operatingsystem {
+		centos: {
+			exec {
+				"sysbench":
+					command => "/usr/bin/yum localinstall -y /tmp/sysbench.rpm",
+					cwd => "/tmp",
+					unless => "/bin/rpm -q sysbench",
+					require => [Package['MySQL-client'], File['/tmp/sysbench.rpm']];
+			}
+			file {
+				"/tmp/sysbench.rpm":
+					source => "puppet:///modules/misc/sysbench-0.5-3.el6_.x86_64.rpm",
+					ensure => present;
+				"/root/sysbench_tests":
+					ensure => link,
+					target => '/usr/share/doc/sysbench/tests',
+					require => Exec['sysbench'];
+			}
+		}
+		ubuntu: {
+			# No Sysbench build for Ubuntu?
+		}
 	}
 }
