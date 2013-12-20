9fa842dc4a2132624e14b1277474c5b8b1a9d023
Jay Janssen <jay.janssen@gmail.com>
2013-12-20 14:04:12 -0500

    support new style of explicit galera versions

--- puppet/modules/percona/manifests/cluster/packages.pp
+++ puppet/modules/percona/manifests/cluster/packages.pp
@@ -13,6 +13,21 @@ class percona::cluster::packages {
 	} elsif $percona_server_version == "-56" {
 		$other_percona_server_version="-55"
 	}
+	
+	# You can set the $galera_version to 2 or 3 for either 55 or 56, but if it is not set it defaults like this:
+	if( $galera_version == undef ) {
+		if( $percona_server_version == "-55" ) {
+			$galera_version = '2'
+		} elsif( $percona_server_version == "-56" ) {
+			$galera_version = '3'
+		}
+	}
+	
+	if( $galera_version == '2' ) {
+		$other_galera_version = '3'
+	} elsif( $galera_version == '3' ) {
+		$other_galera_version = '2'
+	}
 
 
 	case $operatingsystem {
@@ -24,9 +39,10 @@ class percona::cluster::packages {
 					ensure => "absent";
 				"Percona-XtraDB-Cluster-shared$other_percona_server_version.$hardwaremodel":
 					ensure => "absent";
-
+				"Percona-XtraDB-Cluster-galera-$other_galera_version":
+					ensure => "absent";
 				"Percona-XtraDB-Cluster-server$percona_server_version.$hardwaremodel":
-					require => [ Yumrepo['percona'], Package['MySQL-shared'], Package['mysql-libs']],
+					require => [ Yumrepo['percona'], Package['MySQL-shared'], Package['mysql-libs'], Package['galera']],
 					alias => "MySQL-server",
 					ensure => "installed";
 				"Percona-XtraDB-Cluster-client$percona_server_version.$hardwaremodel":
@@ -36,6 +52,9 @@ class percona::cluster::packages {
 				"Percona-XtraDB-Cluster-shared$percona_server_version.$hardwaremodel":
 					require => [ Yumrepo['percona']],
 					alias => "MySQL-shared",
+					ensure => "installed";
+				"Percona-XtraDB-Cluster-galera-$galera_version":
+					alias => "galera",
 					ensure => "installed";
 				# replaces mysql-libs
 				"Percona-Server-shared-51":
