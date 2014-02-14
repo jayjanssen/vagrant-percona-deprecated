28915f5403e97e894ccb3e0e42a415a1d9c8e00f
Jay Janssen <jay.janssen@gmail.com>
2014-02-14 10:11:47 -0500

    add crmsh from opensuse

--- puppet/modules/pacemaker/manifests/packages.pp
+++ puppet/modules/pacemaker/manifests/packages.pp
@@ -6,4 +6,25 @@ class pacemaker::packages {
 			ensure => "installed";
 	}
 
+	# Get crmsh from opensuse repo
+	case $operatingsystem {
+		centos: {
+			yumrepo{ 'network_ha-clustering_Stable':
+				descr => "Stable High Availability/Clustering packages (CentOS_CentOS-6)",
+				gpgcheck => "1",
+				enabled => "1",
+				baseurl => "http://download.opensuse.org/repositories/network:/ha-clustering:/Stable/CentOS_CentOS-6/
+",
+				gpgkey => "http://download.opensuse.org/repositories/network:/ha-clustering:/Stable/CentOS_CentOS-6/repodata/repomd.xml.key"
+				
+			}
+			
+			package {
+				"crmsh":
+					require => Yumrepo['network_ha-clustering_Stable'],
+					ensure => "present";
+			}
+		}
+	}
+
 }
