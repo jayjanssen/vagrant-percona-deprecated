cf44dcfe90bb86ae780b842f4dcf17c4f68f569b
Jay Janssen <jay.janssen@gmail.com>
2014-03-06 10:08:47 -0500

    fixed opensuseha repo filename

--- puppet/modules/pacemaker/manifests/packages.pp
+++ puppet/modules/pacemaker/manifests/packages.pp
@@ -12,9 +12,9 @@ class pacemaker::packages {
 			
 			exec {
 				"opensuse_ha_repo":
-					command => "wget -O opensuse_ha_repo http://download.opensuse.org/repositories/network:/ha-clustering:/Stable/CentOS_CentOS-6/network:ha-clustering:Stable.repo",
+					command => "wget -O opensuse_ha.repo http://download.opensuse.org/repositories/network:/ha-clustering:/Stable/CentOS_CentOS-6/network:ha-clustering:Stable.repo",
 					cwd => "/etc/yum.repos.d",
-					creates => "/etc/yum.repos.d/opensuse_ha_repo",
+					creates => "/etc/yum.repos.d/opensuse_ha.repo",
 					path => ['/bin','/usr/bin','/usr/local/bin']
 			}
 			
