a28a1d6d23495784992e69d6392e299b86885dd9
Jay Janssen <jay.janssen@gmail.com>
2014-04-16 10:33:57 -0400

    spruced up mysql 5.7 install

--- puppet/manifests/mysql_server57.pp
+++ puppet/manifests/mysql_server57.pp
@@ -4,5 +4,17 @@ class { 'mysql::repository':
 }
 
 include mysql::server
+include misc::sysbench
+include misc::mysql_datadir
+include percona::toolkit
+include percona::repository
+include percona::config
+include mysql::service
+include misc::tpcc_mysql
+include misc
 
 Class['mysql::repository'] -> Class['mysql::server']
+Class['misc::mysql_datadir'] -> Class['mysql::server']
+Class['percona::repository'] -> Class['percona::toolkit']
+
+Class['percona::repository'] -> Class['mysql::server'] -> Class['percona::config'] -> Class['mysql::service']
