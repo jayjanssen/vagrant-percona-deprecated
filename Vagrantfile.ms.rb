033670c0d68c7b55338415d6868a7891f054a6f8
Jay Janssen <jay.janssen@gmail.com>
2014-05-26 14:50:39 -0400

    removed extra code block from slave build

--- Vagrantfile.ms.rb
+++ Vagrantfile.ms.rb
@@ -59,8 +59,6 @@ Vagrant.configure("2") do |config|
 	config.ssh.username = "root"
   
 	build_box( config, 'master', '192.168.70.2', '1' )
-	build_box( config, 'slave', '192.168.70.3', '2' ) { |config|
-	  # MS
-	}
+	build_box( config, 'slave', '192.168.70.3', '2' )
 end
 
