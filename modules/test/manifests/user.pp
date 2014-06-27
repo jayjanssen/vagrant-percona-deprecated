b0cc15e7ce1219de12188a34e76c9480552b3372
Jay Janssen <jay.janssen@percona.com>
2014-06-27 09:37:31 -0400

    added sysbench loading and test user creation modules/manifests

---
+++ modules/test/manifests/user.pp
@@ -0,0 +1,18 @@
+class test::user {
+	package { 'percona-toolkit': ensure => installed }
+	
+	exec{ 
+		'create_test_global_user':
+			command => "mysql -e \"GRANT ALL PRIVILEGES ON *.* TO 'test'@'%' IDENTIFIED BY 'test'\"",
+			cwd => '/root',
+			unless => "pt-show-grants | grep \"GRANT ALL PRIVILEGES ON *.* TO 'test'@'%'\"",
+			path => ['/usr/bin', '/bin'],
+			require => Package['percona-toolkit'];
+		'create_test_localhost_user':
+			command => "mysql -e \"GRANT ALL PRIVILEGES ON *.* TO 'test'@'localhost' IDENTIFIED BY 'test'\"",
+			cwd => '/root',
+			unless => "pt-show-grants | grep \"GRANT ALL PRIVILEGES ON *.* TO 'test'@'localhost'\"",
+			path => ['/usr/bin', '/bin'],
+			require => Package['percona-toolkit'];
+	}
+}
