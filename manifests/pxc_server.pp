992000b3d7a129a2a9104a98c4b03efc2ff0417f
Jay Janssen <jay.janssen@percona.com>
2014-07-25 16:21:21 -0400

    configure sst user in pxc server

--- manifests/pxc_server.pp
+++ manifests/pxc_server.pp
@@ -4,10 +4,12 @@ include percona::cluster::client
 include percona::cluster::server
 include percona::cluster::config
 include percona::cluster::service
+include percona::cluster::sstuser
 
 
 include mysql::datadir
 
 Class['mysql::datadir'] -> Class['percona::cluster::server']
 
-Class['percona::repository'] -> Class['percona::cluster::client'] -> Class['percona::cluster::server'] -> Class['percona::cluster::config'] -> Class['percona::cluster::service']
+Class['percona::repository'] -> Class['percona::cluster::client'] -> Class['percona::cluster::server'] -> Class['percona::cluster::config'] -> Class['percona::cluster::service'] -> Class['percona::cluster::sstuser']
+
