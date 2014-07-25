f980de8ca3a46a61f636e2f87127dc329f90ab6b
Jay Janssen <jay.janssen@percona.com>
2014-07-25 16:22:01 -0400

    make bootstrap servers smart enough to restart gracefully if the cluster was already up

--- modules/percona/manifests/cluster/service.pp
+++ modules/percona/manifests/cluster/service.pp
@@ -5,25 +5,29 @@ class percona::cluster::service {
 	# MySQL is not running, it will rebootstrap and potentially create 
 	# a new cluster. This can have nasty consequences for your environment
 	if( $pxc_bootstrap_node == "true" or $pxc_bootstrap_node == true) {
-		$pxc_service_command = "bootstrap-pxc"
-	} else {
-		$pxc_service_command = "start"
-	}
-
-
-	# We do not use the redhat provider but the old fashioned init scripts 
-	# by using the 'base' provider. this allows sending pxc-bootstrap as 
-	# command instead of 'start'
-	service {
-		"mysql":
-			enable  	=> true,
-			ensure  	=> 'running',
-			provider	=> 'base',
-			status		=> "/etc/init.d/mysql status",
-			start		=> "/etc/init.d/mysql $pxc_service_command",
-			stop		=> "/etc/init.d/mysql stop",
-			require 	=> Package['MySQL-server'],
-			subscribe 	=> File["/etc/my.cnf"];
+		# We do not use the redhat provider but the old fashioned init scripts 
+		# by using the 'base' provider. this allows sending pxc-bootstrap as 
+		# command instead of 'start'
+		service {
+			"mysql":
+				enable  	=> true,
+				ensure  	=> 'running',
+				provider	=> 'base',
+				status		=> "/etc/init.d/mysql status",
+				start		=> "(test -f /var/lib/mysql/grastate.dat && /etc/init.d/mysql start) || /etc/init.d/mysql bootstrap-pxc",
+				stop		=> "/etc/init.d/mysql stop",
+				require 	=> Package['MySQL-server'],
+				subscribe 	=> File["/etc/my.cnf"];
 		
+		}
+	} else {
+		service {
+			'mysql':
+				ensure => 'running',
+				subscribe 	=> File["/etc/my.cnf"];
+		}
 	}
+
+
+
 }
