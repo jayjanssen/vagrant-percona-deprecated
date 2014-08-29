7f359e3086a584288aab6cfcca1c4a9e746c3b81
Jay Janssen <jay.janssen@percona.com>
2014-08-29 13:31:55 -0400

    PXC w/ auto-provisioning

--- Vagrantfile.pxc.rb
+++ Vagrantfile.pxc.rb
@@ -31,44 +31,53 @@ pxc_nodes = {
 	}
 }
 
+$cluster_address = "gcomm://" + pxc_nodes.keys().join(',')
 
 Vagrant.configure("2") do |config|
 	config.vm.box = "perconajayj/centos-x86_64"
 	config.ssh.username = "root"
+
+    config.hostmanager.enabled = true # Disable for AWS
+    config.hostmanager.include_offline = true
 
 	# Create all three nodes identically except for name and ip
 	pxc_nodes.each_pair { |name, node_params|
 		config.vm.define name do |node_config|
 			node_config.vm.hostname = name
 			node_config.vm.network :private_network, ip: node_params['local_vm_ip']
+
+			node_config.vm.provision :hostmanager
 			
 			# Provisioners
-			provision_puppet( config, "base.pp" )
-			provision_puppet( config, "pxc_server.pp" ) { |puppet|	
+			provision_puppet( node_config, "base.pp" )
+			provision_puppet( node_config, "pxc_server.pp" ) { |puppet|	
 				puppet.facter = {
 					"percona_server_version"	=> mysql_version,
 					'innodb_buffer_pool_size' => '128M',
 					'innodb_log_file_size' => '64M',
-					'innodb_flush_log_at_trx_commit' => '0'
+					'innodb_flush_log_at_trx_commit' => '0',
+					'pxc_bootstrap_node' => (name == 'node1' ? true : false ),
+					'extra_mysqld_config' => "
+wsrep_provider_options = \"ist.recv_addr=#{name}\"
+wsrep_node_address = #{name}
+wsrep_cluster_address = #{$cluster_address}
+"
 				}
 			}
-			provision_puppet( config, "pxc_client.pp" ) { |puppet|
-				puppet.facter = {
-					"percona_server_version"	=> mysql_version
-				}
-			}
-			provision_puppet( config, "sysbench.pp" )
-			provision_puppet( config, "percona_toolkit.pp" )
-			provision_puppet( config, "myq_gadgets.pp" )
-	
+			provision_puppet( node_config, "percona_toolkit.pp" )
+			provision_puppet( node_config, "myq_gadgets.pp" )
+
+			provision_puppet( node_config, "sysbench.pp" )
+			provision_puppet( node_config, "test_user.pp" )
+
 			# Providers
-			provider_virtualbox( name, config, 256 ) { |vb, override|
+			provider_virtualbox( name, node_config, 256 ) { |vb, override|
 				provision_puppet( override, "pxc_server.pp" ) {|puppet|
 					puppet.facter = {"datadir_dev" => "dm-2"}
 				}
 			}
 	
-			provider_aws( "PXC #{name}", config, 'm1.small', node_params['aws_region'], node_params['security_groups']) { |aws, override|
+			provider_aws( "PXC #{name}", node_config, 'm1.small', node_params['aws_region'], node_params['security_groups']) { |aws, override|
 				aws.block_device_mapping = [
 					{
 						'DeviceName' => "/dev/sdb",
