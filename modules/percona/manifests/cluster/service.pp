a9a837c9623ab988e80941f727940853487456f9
Jay Janssen <jay.janssen@percona.com>
2014-10-24 10:25:54 -0400

    Use systemctl on Centos 7

--- modules/percona/manifests/cluster/service.pp
+++ modules/percona/manifests/cluster/service.pp
@@ -8,17 +8,32 @@ class percona::cluster::service {
 		# We do not use the redhat provider but the old fashioned init scripts 
 		# by using the 'base' provider. this allows sending pxc-bootstrap as 
 		# command instead of 'start'
-		service {
-			"mysql":
-				enable  	=> true,
-				ensure  	=> 'running',
-				provider	=> 'base',
-				status		=> "/etc/init.d/mysql status",
-				start		=> "(test -f /var/lib/mysql/grastate.dat && /etc/init.d/mysql start) || /etc/init.d/mysql bootstrap-pxc",
-				stop		=> "/etc/init.d/mysql stop",
-				require 	=> Package['MySQL-server'],
-				subscribe 	=> File["/etc/my.cnf"];
 		
+		if( $operatingsystem == 'centos' and $operatingsystemrelease =~ /^7/ ) {  #7.0.1406
+			service {
+				"mysql":
+					enable  	=> true,
+					ensure  	=> 'running',
+					provider	=> 'base',
+					status		=> "/etc/init.d/mysql status",
+					start		=> "(test -f /var/lib/mysql/grastate.dat && systemctl start mysql) || systemctl start mysql@bootstrap",
+					stop		=> "systemctl stop mysql",
+					require 	=> Package['MySQL-server'],
+					subscribe 	=> File["/etc/my.cnf"];		
+			}
+		} else {
+			
+			service {
+				"mysql":
+					enable  	=> true,
+					ensure  	=> 'running',
+					provider	=> 'base',
+					status		=> "/etc/init.d/mysql status",
+					start		=> "(test -f /var/lib/mysql/grastate.dat && /etc/init.d/mysql start) || /etc/init.d/mysql bootstrap-pxc",
+					stop		=> "/etc/init.d/mysql stop",
+					require 	=> Package['MySQL-server'],
+					subscribe 	=> File["/etc/my.cnf"];		
+			}
 		}
 	} else {
 		service {
