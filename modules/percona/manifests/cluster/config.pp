3e606767b9ddb65f48d12997f440704b828a6316
Jay Janssen <jay.janssen@percona.com>
2014-11-20 09:51:13 -0500

    Cleaned up percona::cluster::config

--- modules/percona/manifests/cluster/config.pp
+++ modules/percona/manifests/cluster/config.pp
@@ -1,23 +1,4 @@
 class percona::cluster::config {
-
-	if( $wsrep_provider_options == undef ) {
-		$wsrep_provider_options = ""
-	}
-	if( $wsrep_slave_threads == undef ) {
-		$wsrep_slave_threads = 4
-	}
-	if( $wsrep_auto_increment_control == undef ) {
-		$wsrep_auto_increment_control = ON
-	}
-
-	if( $innodb_flush_log_at_trx_commit == undef ) {
-		$innodb_flush_log_at_trx_commit = '1'
-	}
-
-	if( $extra_mysqld_config == undef ) {
-			$extra_mysqld_config = ''
-	}
-
 	file {
 		"/etc/my.cnf":
 			ensure  => present,
