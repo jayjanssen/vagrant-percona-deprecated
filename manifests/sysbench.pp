6401ce4a2abebac43f69c5aeb796a1d9a72f7b28
Jay Janssen <jay.janssen@percona.com>
2014-11-21 10:17:05 -0500

    Sysbench manifest builds whole server

--- manifests/sysbench.pp
+++ manifests/sysbench.pp
@@ -1,2 +1,31 @@
+include base::packages
+include base::insecure
+
+include percona::repository
 include percona::sysbench
 include test::sysbench_test_script
+
+Class['percona::repository'] -> Class['percona::sysbench']
+
+if $enable_consul == 'true' {
+	info( 'enabling consul agent' )
+	
+	$config_hash = delete_undef_values( {
+        'datacenter'  => $datacenter,
+        'data_dir'    => '/opt/consul',
+        'log_level'   => 'INFO',
+        'node_name'   => $node_name,
+		'bind_addr'   => $bind_addr,
+        'client_addr' => '0.0.0.0',
+	})
+	
+	
+	class { 'consul':
+		join_cluster => $join_cluster,
+	    config_hash => $config_hash
+	}
+
+	include consul::local_dns
+	
+	Class['consul::local_dns'] -> Class['test::sysbench_test_script']
+}
