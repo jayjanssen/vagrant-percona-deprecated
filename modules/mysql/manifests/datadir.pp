7f3743e52ba02ea5b92e184b0151fbc331d3a369
Jay Janssen <jay.janssen@percona.com>
2014-12-03 08:19:22 -0500

    'crappy' datadir support

--- modules/mysql/manifests/datadir.pp
+++ modules/mysql/manifests/datadir.pp
@@ -1,36 +1,61 @@
 class mysql::datadir {	
-
 	# Need to set $datadir_dev from Vagrantfile for this to work right
-	package {
-		'xfsprogs': ensure => 'present';
-	}
 
-	exec {
-		"mkfs_mysql_datadir":
-			command => "mkfs.xfs -f /dev/$datadir_dev",
-			require => Package['xfsprogs'],
-			path => "/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin
-",
-			unless => "mount | grep '/var/lib/mysql'";
+    if( $datadir_crappy )  {
+    	exec {
+    		"mkfs_mysql_datadir":
+    			command => "mkfs.ext3 /dev/$datadir_dev",
+    			path => "/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin",
+    			unless => "mount | grep '/var/lib/mysql'";
+    	}
+    	mount {
+    		"/var/lib/mysql":
+    			ensure => "mounted",
+    			device => "/dev/$datadir_dev",
+    			fstype => 'ext3',
+                options => 'defaults',
+    			atboot => "true",
+    			require => Exec["mkfs_mysql_datadir", "mkdir_mysql_datadir"];
+    	}
+    } else {
+    	package {
+    		'xfsprogs': ensure => 'present';
+    	}
+    	exec {
+    		"mkfs_mysql_datadir":
+    			command => "mkfs.xfs -f /dev/$datadir_dev",
+    			require => Package['xfsprogs'],
+    			path => "/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin",
+    			unless => "mount | grep '/var/lib/mysql'";
+    	}
+    	mount {
+    		"/var/lib/mysql":
+    			ensure => "mounted",
+    			device => "/dev/$datadir_dev",
+    			fstype => "xfs",
+    			options => "noatime",
+    			atboot => "true",
+    			require => Exec["mkfs_mysql_datadir", "mkdir_mysql_datadir"];
+
+    	}
+    	# IO scheduler
+    	exec {
+    		"datadir_dev_noop":
+    			command => "echo 'noop' > /sys/block/$datadir_dev/queue/scheduler",
+    			path => "/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin",
+    			unless => "grep -E '\\[noop\\]|none' /sys/block/$datadir_dev/queue/scheduler";
+    	}
+    }
+    
+    exec {
 		"mkdir_mysql_datadir":
 			command => "mkdir /var/lib/mysql",
-			path => "/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin
-",
+			path => "/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin",
 			unless => "test -d /var/lib/mysql";
 
-	}
+    }
 
 
-	mount {
-		"/var/lib/mysql":
-			ensure => "mounted",
-			device => "/dev/$datadir_dev",
-			fstype => "xfs",
-			options => "noatime",
-			atboot => "true",
-			require => Exec["mkfs_mysql_datadir", "mkdir_mysql_datadir"];
-
-	}
 
 	# recreate the mysql datadir if it doesn't exist but mysql_install_db is present
 	# If mysql hasn't been installed yet, this will not run and let the package install create the datadir
@@ -43,13 +68,7 @@ class mysql::datadir {
 			onlyif => "which mysql_install_db && test ! -f /var/lib/mysql/mysql/user.frm";
 	}
 
-	# IO scheduler
-	exec {
-		"datadir_dev_noop":
-			command => "echo 'noop' > /sys/block/$datadir_dev/queue/scheduler",
-			path => "/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin",
-			unless => "grep -E '\\[noop\\]|none' /sys/block/$datadir_dev/queue/scheduler";
-	}
+
 
 	# mount
 
