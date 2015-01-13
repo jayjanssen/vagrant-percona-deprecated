89c6faa736439cd200d8f1e6355ae444573088b2
Kenny Gryp <kenny.gryp@percona.com>
2015-01-13 16:13:23 +0100

    new clustercheck which is compatible with centos7 package name dependency on python-twisted-web

--- modules/percona/manifests/pxc-clustercheck.pp
+++ modules/percona/manifests/pxc-clustercheck.pp
@@ -1,4 +1,4 @@
-# This is an rpm generated from https://github.com/redsox38/clustercheck/tree/redsox38
+# This is an rpm generated from https://github.com/grypyrg/clustercheck/tree/centos7
 # it has a better name, init script and rpm spec.
 
 class percona::pxc-clustercheck {	
@@ -7,16 +7,23 @@ class percona::pxc-clustercheck {
 			command => "/usr/bin/yum localinstall -y /tmp/percona-clustercheck.rpm",
 			cwd => "/tmp",
 			unless => "/bin/rpm -q percona-clustercheck",
-			require => [File['/tmp/percona-clustercheck.rpm']];
+			require => [ File['/tmp/percona-clustercheck.rpm'], Package["MySQL-python"], Package["python-twisted-web"] ];
 	}
 	file {
 		"/tmp/percona-clustercheck.rpm":
-			source => "puppet:///modules/percona/percona-clustercheck-1.0-0.noarch.rpm",
+			source => "puppet:///modules/percona/percona-clustercheck-1.0-2.noarch.rpm",
 			ensure => present;
 	}
 	service {
 		"percona-clustercheck":
 			ensure => running,
 			subscribe => Exec['percona-clustercheck'];
+	}
+
+	package {
+		"MySQL-python":
+			ensure => installed;
+		"python-twisted-web":
+			ensure => installed
 	}
 }
