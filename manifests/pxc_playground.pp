757c7b760c15a96622cb5b897f2d936eda8ce14b
Kenny Gryp <kenny.gryp@percona.com>
2015-01-13 16:11:28 +0100

    sysbench and users dependencies

--- manifests/pxc_playground.pp
+++ manifests/pxc_playground.pp
@@ -1,6 +1,8 @@
 # different tools which are used to setup lot's of stuff for pxc and to test/train...
 
 include test::sysbench_custom_lua
+
+Class['test::user'] -> Class['percona::cluster::service']
 include test::user
 
 
@@ -10,7 +12,7 @@ include percona::cluster::client
 
 Class['percona::repository'] -> Class['percona::cluster::client']
 
-
+Class['test::sysbench_pkg'] -> Class['percona::cluster::client']
 include test::sysbench_pkg
 
 
