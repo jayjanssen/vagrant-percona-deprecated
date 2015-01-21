6855bfa4c079b51f204d3d46d3ac4e5d18927abf
Jay Janssen <jay.janssen@percona.com>
2015-01-21 16:43:47 -0500

    add reload sysbench script to get fresh data quickly

--- modules/test/manifests/sysbench_test_script.pp
+++ modules/test/manifests/sysbench_test_script.pp
@@ -1,6 +1,14 @@
 class test::sysbench_test_script {
-    if !$mysql_host { $mysql_host = 'localhost' }
-    
+	if !$mysql_host { $mysql_host = 'localhost' }
+
+	file {
+		'/usr/local/bin/run_sysbench_reload.sh':
+			ensure => present,
+			content => "sysbench --db-driver=mysql --test=/usr/share/doc/sysbench/tests/db/oltp.lua --mysql-user=test --mysql-password=test --mysql-db=$schema --mysql-host=$mysql_host --oltp-tables-count=$tables cleanup
+sysbench --test=sysbench_tests/db/parallel_prepare.lua --db-driver=mysql --mysql-user=root --mysql-db=$schema --oltp-tables-count=$tables --oltp-table-size=$rows --oltp-auto-inc=off --num-threads=$threads run",
+			mode => 0755;
+		}
+
 	file {
 		'/usr/local/bin/run_sysbench_oltp.sh':
 			ensure => present,
