d71b9afcb930fde81b94badef08f0073e787e7b5
Jay Janssen <jay.janssen@percona.com>
2015-02-09 10:41:01 -0500

    updated myq_tools to 0.4

--- modules/misc/manifests/myq_tools.pp
+++ modules/misc/manifests/myq_tools.pp
@@ -1,7 +1,7 @@
 class misc::myq_tools {
     exec {
     		"myq_tools":
-    			command => "wget https://github.com/jayjanssen/myq-tools/releases/download/v0.3/myq_tools.tgz && tar xvzf myq_tools.tgz -C /usr/local/bin --strip-components=1 && ln -sf /usr/local/bin/myq_status.linux-amd64 /usr/local/bin/myq_status",
+    			command => "wget https://github.com/jayjanssen/myq-tools/releases/download/v0.4/myq_tools.tgz && tar xvzf myq_tools.tgz -C /usr/local/bin --strip-components=1 && ln -sf /usr/local/bin/myq_status.linux-amd64 /usr/local/bin/myq_status",
     			cwd => "/tmp",
     			creates => "/usr/local/bin/myq_status.linux-amd64",
     			path => ['/bin','/usr/bin','/usr/local/bin'],
