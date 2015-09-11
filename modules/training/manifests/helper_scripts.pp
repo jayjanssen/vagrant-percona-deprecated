342ec064ac0ce0d1232f9bd3a229e795e6b297d2
Kenny Gryp <kenny.gryp@percona.com>
2015-09-11 14:11:59 +0200

    run_app.sh helps our training/tutorials to simulate load on an 'app'

--- modules/training/manifests/helper_scripts.pp
+++ modules/training/manifests/helper_scripts.pp
@@ -6,7 +6,11 @@ class training::helper_scripts {
 		"/root/bin/ssh_keygen_and_distribute.sh":
 			ensure	=> present,
 			content	=> template("training/ssh_keygen_and_distribute.sh.erb"),
-			mode	=> 755
+			mode	=> 755;
+		"/root/bin/run_app.sh":
+			ensure	=> present,
+			content	=> template("training/run_app.sh.erb"),
+			mode	=> 755;
 	}
 
 	package {
