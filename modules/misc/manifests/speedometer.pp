ce4ff90752615d29a036c8591ac67988d83231a9
Kenny Gryp <kenny.gryp@percona.com>
2015-09-20 02:33:33 +0200

    added speedometer package isntall

---
+++ modules/misc/manifests/speedometer.pp
@@ -0,0 +1,23 @@
+class misc::speedometer {
+	
+	case $operatingsystem {
+		centos: {
+			package {
+				'python-urwid': ensure => 'present';
+			}
+
+			file {
+				'/root/bin/speedometer':
+					ensure	=> present,
+					mode 	=> 0755,
+					source  => "puppet:///misc/speedometer.py"
+			}
+		}
+		ubuntu: {
+			package {
+				"speedometer":
+					ensure	=> installed;
+			}
+		}
+	}
+}
