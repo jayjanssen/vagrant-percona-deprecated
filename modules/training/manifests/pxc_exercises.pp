cce3fbdffecf4d30d0d4e6f99158e96ac965f37f
Kenny Gryp <kenny.gryp@percona.com>
2015-09-20 02:34:06 +0200

    script to add latency to the last node provisioned

--- modules/training/manifests/pxc_exercises.pp
+++ modules/training/manifests/pxc_exercises.pp
@@ -5,6 +5,10 @@ class training::pxc_exercises {
 			ensure	=> present,
 			content	=> template('training/reproduce_lcf.sh.erb'),
 			mode	=> 0755;
+		"/root/bin/last_node_to_dc2.sh":
+			ensure	=> present,
+			mode 	=> 0755,
+			content	=> template('training/last_node_to_dc2.sh.erb');
 	}
 
 }
