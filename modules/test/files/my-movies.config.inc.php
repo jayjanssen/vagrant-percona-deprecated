097a45faa6e70d41e76f9ee348546438e7ec5963
Kenny Gryp <kenny.gryp@percona.com>
2016-02-07 22:44:04 +0000

    override config.inc.php of my-movies as it has db1 hardcoded, does not work in these environments, localhost is better

---
+++ modules/test/files/my-movies.config.inc.php
@@ -0,0 +1,14 @@
+<?php
+
+define('BASE_URI', '/my-movies/');
+
+define('MYSQL_HOST', 'localhost');
+define('MYSQL_USER', 'app');
+define('MYSQL_DATABASE', 'imdb');
+define('MYSQL_PASSWORD', 'pass');
+
+define('DEBUG_MODE', true);
+
+error_reporting(E_ALL ^ E_NOTICE);
+
+?>
