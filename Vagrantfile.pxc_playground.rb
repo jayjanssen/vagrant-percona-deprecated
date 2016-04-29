0892ab4bf26733ea53fbbdb4fa7bd4305519db89
Kenny Gryp <kenny.gryp@percona.com>
2016-04-28 20:36:08 -0700

    updated vagrantfile with the vagrantfile used in advanced pxc tutorial

--- Vagrantfile.pxc_playground.rb
+++ Vagrantfile.pxc_playground.rb
@@ -12,10 +12,11 @@
 # # vagrant up --no-provision --parallel
 # # vagrant provision --parallel
 
-prefix='team1-'
-iprange='192.168.60'
-if_adapter='vboxnet4'
+prefix='cluster-'
+iprange='192.168.56'
+if_adapter='vboxnet0'
 
+enable_repo_percona_testing=true
 
 require File.dirname(__FILE__) + '/lib/vagrant-common.rb'
 
@@ -29,24 +30,37 @@ mysql_version = "56"
 pxc_nodes = {
 	prefix + 'pxc1' => {
 		'local_vm_ip' => iprange + '.2',
-		'aws_region' => 'eu-west-1',
+		'aws_region' => 'us-east-1',
 		'security_groups' => ['default','pxc', 'haproxy'],
+		'haproxy_disabled' => 'false',
+		'maxscale_disabled' => 'false',
 		'haproxy_primary' => true,
-		'pxc_bootstrap_node' => true
+		'pxc_bootstrap_node' => true,
+		'server_id'	=> '1'
 	},
 	prefix + 'pxc2' => {
 		'local_vm_ip' => iprange + '.3',
-		'aws_region' => 'eu-west-1',
+		'aws_region' => 'us-east-1',
 		'security_groups' => ['default','pxc', 'haproxy'],
-		'pxc_bootstrap_node' => false
+		'pxc_bootstrap_node' => false,
+		'haproxy_disabled' => 'true',
+		'maxscale_disabled' => 'true',
+		'server_id'	=> '2'
 	},
 	prefix + 'pxc3' => {
 		'local_vm_ip' => iprange + '.4',
-		'aws_region' => 'eu-west-1',
+		'aws_region' => 'us-east-1',
 		'security_groups' => ['default','pxc', 'haproxy'],
-		'pxc_bootstrap_node' => false
+		'pxc_bootstrap_node' => false,
+		'haproxy_disabled' => 'true',
+		'maxscale_disabled' => 'true',
+		'server_id'	=> '3'
 	}
 }
+
+
+# make a comma separated serverlist, which can be reused in puppet
+serverlist=pxc_nodes.map{|k,v| "#{k}"}.join(',')
 
 # should we use the public or private ips when using AWS
 hostmanager_aws_ips='private'
@@ -70,6 +84,17 @@ Vagrant.configure("2") do |config|
 			node_config.vm.hostname = name
 			node_config.vm.network :private_network, ip: node_params['local_vm_ip'], adaptor: if_adapter
 
+			# this is to test haproxy PROXY support in PXC
+			if name == prefix + 'pxc1'
+				node_config.vm.network :private_network, ip: '172.0.0.1'
+			end
+
+            ssh_port = "882" + node_params["server_id"]
+            haproxy_port = "888" + node_params["server_id"]
+
+            node_config.vm.network "forwarded_port", guest: 22, host: ssh_port, auto_correct: false
+            node_config.vm.network "forwarded_port", guest: 8080, host: haproxy_port, auto_correct: false
+
 			# custom port forwarding
 			node_config.vm.network "forwarded_port", guest: 8080, host: 8080, auto_correct: true	
 
@@ -78,12 +103,16 @@ Vagrant.configure("2") do |config|
 
 			provision_puppet( node_config, "pxc_playground.pp" ) { |puppet|
 				puppet.facter = {
-					'vagrant_hostname'				=> name,
-					"haproxy_servers"					=> pxc_nodes.map{|k,v| "#{k}"}.join(','),
-					"haproxy_servers_primary"			=> pxc_nodes.select{|k,v| ! v.select{|k2,v2| k2=="haproxy_primary" && v2==true}.empty? }.map{|k3,v3| "#{k3}"}.join(','),
-					"maxscale_servers"                  => pxc_nodes.map{|k,v| "#{k}"}.join(','),
-					"datadir_dev" 						=> "dm-2",
+					'vagrant_hostname'					=> name,
 					"percona_server_version"			=> mysql_version,
+					"haproxy_servers"					=> serverlist,
+					"haproxy_disabled"					=> node_params['haproxy_disabled'],
+					"maxscale_disabled"					=> node_params['maxscale_disabled'],
+					"haproxy_servers_primary"			=> pxc_nodes.select{|k,v| ! v.select{|k2,v2| k2=="haproxy_primary" && v2==true}.empty? }.map{|k3,v3| "#{k3}"}.join(','),
+                    "maxscale_servers"                  => serverlist,
+                    "cluster_servers"                   => serverlist,
+					"datadir_dev" 						=> "dm-2",
+					'datadir_fs'						=> "xfs",
 					'percona_agent_enabled'             => percona_agent_enabled,
 					'percona_agent_api_key'				=> percona_agent_api_key,
 					'innodb_buffer_pool_size' 			=> '128M',
@@ -92,7 +121,11 @@ Vagrant.configure("2") do |config|
 					'pxc_bootstrap_node'				=> node_params['pxc_bootstrap_node'],
 					'extra_mysqld_config'				=> 
 						'wsrep_cluster_address=gcomm://' + pxc_nodes.map{|k,v| "#{k}"}.join(',') + "\n" +
-						"wsrep_sst_receive_address=" + name + "\n"
+						"wsrep_sst_receive_address=" + name + "\n" +
+						"wsrep_node_address=" + name + "\n" +
+                        "log_slave_updates\n" +
+						"server_id=" + node_params['server_id'] + "\n" +
+						"log_bin" + "\n"
 				}
 			}
 
@@ -103,26 +136,27 @@ Vagrant.configure("2") do |config|
 
 
 			# Providers
-			provider_virtualbox( name, node_config, 512) { |vb, override|
+			provider_virtualbox( name, node_config, 256) { |vb, override|
 				provision_puppet( override, "pxc_playground.pp" ) {|puppet|
 					puppet.facter = {"datadir_dev" => "dm-2"}
 				}
 			}
 	
-			provider_aws( "PXC #{name}", node_config, 't2.small', node_params['aws_region'], node_params['security_groups'], hostmanager_aws_ips) { |aws, override|
+			provider_aws( "PXC #{name}", node_config, 'm1.small', node_params['aws_region'], node_params['security_groups'], hostmanager_aws_ips) { |aws, override|
 				aws.block_device_mapping = [
 					{
-						'DeviceName' => "/dev/sdb",
+						'DeviceName' => "/dev/xvdf",
 						'VirtualName' => "ephemeral0"
 					}
 				]
 				provision_puppet( override, "pxc_playground.pp" ) {|puppet|
-					puppet.facter = {"datadir_dev" => "xvdb"}
+					puppet.facter = {"datadir_dev" => "xvdf"}
 				}
 
 			}
 
 		end
 	}
+
 end
 
